96ae00ea6b393c8ea7e45e8f18bf9fa55de20618
Rakibul1294 <md.rakibul.1294@gmail.com>
2020-04-09 23:43:22 +0600

    doctors-portal node api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 require('dotenv').config();
 
 const app = express();
@@ -13,13 +14,12 @@ app.use(bodyParser.json());
 const uri = process.env.DB_PATH;
 
 let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const users = ["Asad", 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
 
 
-app.get('/foodItems', (req, res) => {
+app.get('/appointments', (req, res) => {
     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("onlineFoodStore").collection("foodType");
+        const collection = client.db("doctorPortal").collection("docAppointList");
         collection.find().toArray((err, documents) => {
             if (err) {
                 console.log(err);
@@ -33,37 +33,11 @@ app.get('/foodItems', (req, res) => {
     });
 });
 
-app.get('/foodItem/:key', (req, res) => {
-    const key = req.params.key;
-
+app.get('/appointedPeople', (req, res) => {
     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("onlineFoodStore").collection("foodType");
-        collection.find({key}).toArray((err, documents) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ message: err });
-            }
-            else {
-                res.send(documents[0]);
-            }
-        });
-        // client.close();
-    });
-});
-
-//delete
-//update
-// post
-app.post('/getfoodItemsByKey', (req, res) => {
-    const key = req.params.key;
-    const productKeys = req.body;
-    console.log(productKeys);
-
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("onlineFoodStore").collection("foodType");
-        collection.find({ key: { $in: productKeys } }).toArray((err, documents) => {
+        const collection = client.db("doctorPortal").collection("appointedPeople");
+        collection.find().toArray((err, documents) => {
             if (err) {
                 console.log(err);
                 res.status(500).send({ message: err });
@@ -76,12 +50,36 @@ app.post('/getfoodItemsByKey', (req, res) => {
     });
 });
 
-app.post('/addfoodItems', (req, res) => {
-    const product = req.body;
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+app.get('/appointedPeople/:key', (req, res) => {
+    const key = req.params.key;
+    console.log(key);
+
+    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("onlineFoodStore").collection("foodType");
-        collection.insert(product, (err, result) => {
+        const collection = client.db("doctorPortal").collection("appointedPeople");
+        collection.find({ joinAppointDate: key }).toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            }
+            else {
+                console.log(documents);
+                res.send(documents);
+            }
+        });
+        // client.close();
+    });
+});
+
+//delete
+//update
+// post
+app.post('/appointedPeople', (req, res) => {
+    const appointedPeople = req.body;
+    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("doctorPortal").collection("appointedPeople");
+        collection.insert(appointedPeople, (err, result) => {
             if (err) {
                 console.log(err);
                 res.status(500).send({ message: err });
@@ -94,25 +92,76 @@ app.post('/addfoodItems', (req, res) => {
     });
 });
 
-app.post('/placeOrder', (req, res) => {
-    const orderDetails = req.body;
-    orderDetails.orderTime = new Date();
-    console.log(orderDetails);
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+app.post('/prescriptionAdded/:key', (req, res) => {
+    const prescriptionAdded = req.body;
+    console.log(prescriptionAdded);
+    const key = req.params.key;
+    console.log(key);
+    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("onlineFoodStore").collection("orders");
-        collection.insertOne(orderDetails, (err, result) => {
+        const collection = client.db("doctorPortal").collection("appointedPeople");
+        collection.updateOne({ "_id": ObjectID(key) }, { "$set": prescriptionAdded }, { upsert: true }, (err, result) => {
             if (err) {
                 console.log(err);
                 res.status(500).send({ message: err });
             }
             else {
-                res.send(result.ops[0]);
+                // console.log(result);
+                res.send(result);
             }
         });
         // client.close();
     });
 });
+
+
+
+
+
+
+
+
+
+// app.post('/getfoodItemsByKey', (req, res) => {
+//     const key = req.params.key;
+//     const productKeys = req.body;
+//     console.log(productKeys);
+
+//     client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         // const collection = client.db("onlineFoodStore").collection("foodType");
+//         collection.find({ key: { $in: productKeys } }).toArray((err, documents) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send({ message: err });
+//             }
+//             else {
+//                 res.send(documents);
+//             }
+//         });
+//         // client.close();
+//     });
+// });
+
+// app.post('/placeOrder', (req, res) => {
+//     const orderDetails = req.body;
+//     orderDetails.orderTime = new Date();
+//     console.log(orderDetails);
+//     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         const collection = client.db("onlineFoodStore").collection("orders");
+//         collection.insertOne(orderDetails, (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send({ message: err });
+//             }
+//             else {
+//                 res.send(result.ops[0]);
+//             }
+//         });
+//         // client.close();
+//     });
+// });
 
 const port = process.env.PORT || 4200;
 app.listen(port, () => console.log('Listenting to port 4200'));
